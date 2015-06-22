7c7468252a879d2b145bfef2bcb35b2417c3f2ef
root <root@ip-10-167-188-175.ec2.internal>
2015-06-22 17:07:54 +0000

    Initial commit

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ from tornado.options import define
 define("port", default=5000, help="run on the given port", type=int)
 
 
+# the main page
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, q):
+       self.write("HELLO WORLD")
+
 # application settings and handle mapping info
 class Application(tornado.web.Application):
     def __init__(self):
@@ -33,30 +38,10 @@ class Application(tornado.web.Application):
         )
         tornado.web.Application.__init__(self, handlers, **settings)
 
-
-# the main page
-class MainHandler(tornado.web.RequestHandler):
-    def get(self, q):
-        if 'GOOGLEANALYTICSID' in os.environ:
-            google_analytics_id = os.environ['GOOGLEANALYTICSID']
-        else:
-            google_analytics_id = False
-
-        self.render(
-            "main.html",
-            page_title='Heroku Funtimes',
-            page_heading='Hi!',
-            google_analytics_id=google_analytics_id,
-        )
-
-
-# RAMMING SPEEEEEEED!
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(tornado.options.options.port)
-
-    # start it up
     tornado.ioloop.IOLoop.instance().start()
 
 
